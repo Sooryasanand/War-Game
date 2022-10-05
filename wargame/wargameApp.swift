1925abfe907127bc31884d2b5d5832b1d9b0ac2a
Sooryasanand <sooryanarayanan.com@gmail.com>
2022-10-05 16:34:16 +1100

    Initial Commit

---
+++ wargame/wargameApp.swift
@@ -0,0 +1,17 @@
+//
+//  wargameApp.swift
+//  wargame
+//
+//  Created by Soorya Narayanan Sanand on 5/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct wargameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
