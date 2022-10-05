764e65e515ae53cef49ad2f7a6a53da46ce989be
Sooryasanand <sooryanarayanan.com@gmail.com>
2022-10-05 18:58:15 +1100

    first commit

--- wargame/ContentView.swift
+++ wargame/ContentView.swift
@@ -8,14 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "card2"
+    @State private var cpuCard = "card5"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill).ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String (cpuRand)
+                    print(playerCard)
+                    
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    }
+                    else if cpuRand > playerRand{
+                        cpuScore += 1
+                    }
+                    
+                }, label: {
+                    Image("dealbutton")
+                })
+
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.title2)
+                            .fontWeight(.heavy)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom)
+                
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .fontWeight(.heavy)
+                            .padding(.bottom)
+                            .font(.title2)
+                            .foregroundColor(Color.white)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+                
+            }
         }
-        .padding()
     }
 }
 
